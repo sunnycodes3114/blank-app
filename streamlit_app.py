6bb771009ebc997d5047534d5b58a4e0b1cc089c
sunnycodes3114 <g11ascsannidhay.tws@gmail.com>
2024-11-30 09:18:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+import os
+import subprocess
+try:
+    import torch
+except ImportError:
+    subprocess.run(["pip", "install", "torch==2.0.1+cpu", "torchvision==0.15.2+cpu", "torchaudio==2.0.2+cpu"])
+
 import streamlit as st
 
 st.title("🎈 My new apfgrfgp")
